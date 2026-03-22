87f40ead55aac33313293469912ac7709e42a11a
Dipal <38803886+pateldipal@users.noreply.github.com>
2026-03-22 15:03:07 +0000

    Operations

--- 01_Basic/02_Datatype.js
+++ 01_Basic/02_Datatype.js
@@ -27,4 +27,38 @@ let isLoggedIn = false;
 let score = "33dsdsd";
 
 let scoreNumber = Number(score);
-console.log(typeof scoreNumber);
+// console.log(typeof scoreNumber);
+
+
+
+
+/*  **********************Operations**************************** */
+
+let value = 5;
+// console.log(2 + 2);
+// console.log(2 * 2);
+// console.log(2 / 2);
+// console.log(2 % 3);
+// console.log(2 - 2);
+// console.log(2 ** 2);
+
+let str1 = "hello";
+let str2 = "dipal";
+
+let str3 = str1 + str2;
+
+// console.log(str3);
+
+// console.log("1" + 2);
+
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+
+let gameCounter = 100;
+
+gameCounter++;
+
+console.log(gameCounter);
+
