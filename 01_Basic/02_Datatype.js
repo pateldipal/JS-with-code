d80bbf4163607ee24577149b66086f2ff7bf2390
Dipal <38803886+pateldipal@users.noreply.github.com>
2026-03-22 14:46:56 +0000

    convert number in javascript

---
+++ 01_Basic/02_Datatype.js
@@ -0,0 +1,30 @@
+const number = 33;
+
+//console.log(number);
+
+
+/* we have two type of DataType (Primitive & Non-Primitive) */
+/* Primitive Datatype  
+    [String :"dipal",
+     number : 125865, 
+     Boolean: true / false,
+     Null : null
+     Undefined : undefined,
+     Symbol: Symbol('foo'),
+     Bigint: 123n  ]
+*/
+
+let Empname = "dipal";
+//console.log(Empname);
+
+let empacnumber = 45865;
+//console.log(empacnumber);
+
+let isLoggedIn = false;
+
+//console.log( isLoggedIn);
+
+let score = "33dsdsd";
+
+let scoreNumber = Number(score);
+console.log(typeof scoreNumber);
