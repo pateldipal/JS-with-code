0cf9a3d8f94a2289d3fba218b874f2c0cc76decd
Dipal <38803886+pateldipal@users.noreply.github.com>
2026-03-23 15:26:36 +0000

    String_funtion

---
+++ 01_Basic/03_String_javascript.js
@@ -0,0 +1,10 @@
+let name = "dipal";
+const repoCount = 20;
+//console.log(name + repoCount + "value");
+
+console.log(`hello my name ${name} and my repo count is ${repoCount}`);
+
+const gameName  = new String('Trishiv');
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
