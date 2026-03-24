5038eeeb8cbbb279cb13e03c7f4e58a5d4f853e8
Dipal <38803886+pateldipal@users.noreply.github.com>
2026-03-24 10:41:25 +0000

    string function

--- 01_Basic/03_String_javascript.js
+++ 01_Basic/03_String_javascript.js
@@ -2,9 +2,30 @@ let name = "dipal";
 const repoCount = 20;
 //console.log(name + repoCount + "value");
 
-console.log(`hello my name ${name} and my repo count is ${repoCount}`);
+// console.log(`hello my name ${name} and my repo count is ${repoCount}`);
 
 const gameName  = new String('Trishiv');
 
-console.log(gameName[0]);
-console.log(gameName.__proto__);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+
+// console.log(gameName.charAt(2));
+// console.log(gameName.substring(0,5));
+
+// const newString = gameName.substring(0,4)
+// console.log(newString);
+
+// console.log(gameName.slice(-7,4));
+
+const newStringOne = "  jignesh  ";
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url =  "http://blog.test%20site.io/dashboard";
+
+console.log(url.replace('%20','-'));
