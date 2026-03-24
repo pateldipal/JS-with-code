16f2cfb14ccee4702c138ca924554c183413a99a
Dipal <38803886+pateldipal@users.noreply.github.com>
2026-03-24 11:03:42 +0000

    Maths

---
+++ 01_Basic/04_Maths.js
@@ -0,0 +1,15 @@
+const score = 400;
+console.log(score);
+
+const balance = new Number(540);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 1235.5544
+
+console.log(otherNumber.toPrecision(4));
+
+const anyNumber = 1000000;
+console.log(anyNumber.);
+
+
